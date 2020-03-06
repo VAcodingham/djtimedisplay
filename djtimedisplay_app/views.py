8c573ba0e55187f397e8727ab1c5fd004ca95607
Rowan Smith <rowan@nomad.local>
2020-03-06 11:15:08 -0500

    initial commit

--- djtimedisplay_app/views.py
+++ djtimedisplay_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from time import gmtime, strftime
-
+from time import localtime, strftime
+import datetime
 def timedisplay(request):
+    
     context = {
-        "datetime": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        "datetime": strftime("%Y-%m-%d %H:%M %p", localtime())
+
     }
     return render(request, 'index.html', context)
 
