c48892b36757e1d911fc73671c138d0b419da686
Rowan Smith <rowan@nomad.local>
2020-03-06 08:12:52 -0500

    initial commit

---
+++ djtimedisplay_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjtimedisplayAppConfig(AppConfig):
+    name = 'djtimedisplay_app'
---
+++ djtimedisplay_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from time import gmtime, strftime
+
+def timedisplay(request):
+    context = {
+        "datetime": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request, 'index.html', context)
+
+
